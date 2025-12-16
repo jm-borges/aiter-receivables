389e9f25505ef2692e26fc870c584a8357a60c3b
João Manoel Borges <jm.borges7312@gmail.com>
2025-12-16 14:20:03 -0300

    fixes business partners form

--- resources/views/components/business-partners/form/phone-input.blade.php
+++ resources/views/components/business-partners/form/phone-input.blade.php
@@ -5,6 +5,6 @@
 
 <div @class(['mb-4' => !$inline])>
     <label for="phone" class="block text-sm font-medium text-gray-700">Telefone</label>
-    <input type="text" name="phone" id="phone" value="{{ old('phone', $partner->phone) }}"
+    <input type="text" name="phone" id="phone" value="{{ old('phone', $partner?->phone) }}"
         class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
 </div>
