095d3d48bcd02d5d1ca7a0351b53353b4131828a
João Manoel Borges <jm.borges7312@gmail.com>
2025-12-18 15:25:03 -0300

    updates scripts

--- public/assets/scripts/common/api.js
+++ public/assets/scripts/common/api.js
@@ -1,8 +1,16 @@
-export const fetchJson = async (url) => {
+export const fetchJson = async (url, options = {}) => {
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
+
     const res = await fetch(url, {
         credentials: 'include',
-        headers: { 'Accept': 'application/json' },
+        headers: {
+            'Accept': 'application/json',
+            'X-CSRF-TOKEN': csrfToken,
+            ...options.headers,
+        },
+        ...options,
     });
+
     if (!res.ok) throw new Error(`Erro ${res.status}`);
     return res.json();
 };
